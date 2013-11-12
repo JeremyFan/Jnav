415e9aa7b605fdcc626e12061fb29ac2705373cd
JeremyFan <jeremyfan_14@live.com>
2013-11-12 18:32:47 +0800

    格式test

--- popup.js
+++ popup.js
@@ -1,41 +1,39 @@
-
-
-window.onload=function(){
+window.onload = function() {
 	ctrlMonitor();
 	setWebsiteData();
 }
 
 /// 记录当前ctrl键是否被按下
-var isPressCtrl=false;
+var isPressCtrl = false;
 /// 监听ctrl键
-function ctrlMonitor(){
-	var body=document.getElementById('b');
-	body.onkeydown=function(e){
-		if(e.ctrlKey)
-			isPressCtrl=true;
+function ctrlMonitor() {
+	var body = document.getElementById('b');
+	body.onkeydown = function(e) {
+		if (e.ctrlKey)
+			isPressCtrl = true;
 	}
-	body.onkeyup=function(e){
-		if(e.ctrlKey)
-			isPressCtrl=true;
+	body.onkeyup = function(e) {
+		if (e.ctrlKey)
+			isPressCtrl = true;
 	}
 }
 
 /// 遍历数据，设置网址
-function setWebsiteData(){
-	var content=document.getElementById('website');
+function setWebsiteData() {
+	var content = document.getElementById('website');
 
-	for(var i in Website){
-		var website=Website[i];
-		var a=document.createElement('a');
-		a.innerHTML=website.text;
-		a.style.backgroundImage=website.logo;
+	for (var i in Website) {
+		var website = Website[i];
+		var a = document.createElement('a');
+		a.innerHTML = website.text;
+		a.style.backgroundImage = website.logo;
 		content.appendChild(a);
 
-		a.onclick=function(){
+		a.onclick = function() {
 			chrome.tabs.create({
-				url:website.url,
+				url: website.url,
 				// isPressCtrl控制打开的tab是否为活动页
-				active:!isPressCtrl
+				active: !isPressCtrl
 			});
 		}
 	}
@@ -43,19 +41,19 @@ function setWebsiteData(){
 
 /// 网址数据
 var Website = {
-	cnBlogs:{
-		text:'博客园',
-		url:'http://www.cnblogs.com',
-		logo:'url(http://static.cnblogs.com/favicon.ico)'
+	cnBlogs: {
+		text: '博客园',
+		url: 'http://www.cnblogs.com',
+		logo: 'url(http://static.cnblogs.com/favicon.ico)'
 	},
-	jQuery:{
-		text:'jQuery',
-		url:'http://jquery.com',
-		logo:'url(http://jquery.com/jquery-wp-content/themes/jquery.com/i/favicon.ico)'
+	jQuery: {
+		text: 'jQuery',
+		url: 'http://jquery.com',
+		logo: 'url(http://jquery.com/jquery-wp-content/themes/jquery.com/i/favicon.ico)'
 	},
-	NEC:{
-		text:'NEC',
-		url:'http://nec.netease.com',
-		logo:'url(http://nec.netease.com/img/favicon.ico)'
+	NEC: {
+		text: 'NEC',
+		url: 'http://nec.netease.com',
+		logo: 'url(http://nec.netease.com/img/favicon.ico)'
 	}
 }
