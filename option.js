75bc115fe9f947e3ef368c27a2b7a37e1863a2dd
JeremyFan <jeremyfan_14@live.com>
2014-07-18 15:05:22 +0800

    分类的增/改

--- option.js
+++ option.js
@@ -5,53 +5,58 @@ window.onload=function(){
 }
 
 function initButtonEvent(){
-	var $btnSiteSave=$('#btnSiteSave'),
-		$btnSiteCancel=$('#btnSiteCancel'),
-		$btnCataSave=$('#btnCataSave'),
-		$btnCataCancel=$('#btnCataCancel');
-
-	$btnSiteSave.onclick=function(){
-		var name=document.querySelector('.sitebox .name').value,
-			url=document.querySelector('.sitebox .url').value,
-			catalog=document.querySelector('.sitebox .catalog').value;
-			obj={};
-
-		obj[name]={
-			name:name,
-			url:url,
-			catalog:catalog,
-			type:'site'
-			
-		}
-		chrome.storage.sync.set(obj,function(){
-			alert('set okay.')
-		})
-	}
-
-	$btnCataSave.onclick=function(){
-		var name=document.querySelector('.catabox .name').value,
-			obj={};
-			obj[name]={
-				name:name,
-				type:'cata'
-			};
-
-		chrome.storage.sync.set(obj,function(){
-			alert('set okay');
-		})
-	}
-
-	$btnCataCancel.onclick=function(){
-		var box=document.querySelector('.catabox');
-		box.removeClass('active');
-	}
-
-	$('#btnCataSee').onclick=function(){
-		chrome.storage.sync.get(null,function(result){
-			debugger
-		})
+	var btnCataCreate=document.querySelector('#btnCataCreate');
+	btnCataCreate.onclick=function(){
+		showCatabox(this);
 	}
 }
+// 	var $btnSiteSave=$('#btnSiteSave'),
+// 		$btnSiteCancel=$('#btnSiteCancel'),
+// 		$btnCataSave=$('#btnCataSave'),
+// 		$btnCataCancel=$('#btnCataCancel');
+
+// 	$btnSiteSave.onclick=function(){
+// 		var name=document.querySelector('.sitebox .name').value,
+// 			url=document.querySelector('.sitebox .url').value,
+// 			catalog=document.querySelector('.sitebox .catalog').value;
+// 			obj={};
+
+// 		obj[name]={
+// 			name:name,
+// 			url:url,
+// 			catalog:catalog,
+// 			type:'site'
+			
+// 		}
+// 		chrome.storage.sync.set(obj,function(){
+// 			alert('set okay.')
+// 		})
+// 	}
+
+// 	$btnCataSave.onclick=function(){
+// 		var name=document.querySelector('.catabox .name').value,
+// 			obj={};
+// 			obj[name]={
+// 				name:name,
+// 				type:'cata'
+// 			};
+
+// 		chrome.storage.sync.set(obj,function(){
+// 			alert('set okay');
+// 		})
+// 	}
+
+// 	$btnCataCancel.onclick=function(){
+// 		var box=document.querySelector('.catabox');
+// 		box.removeClass('active');
+// 	}
+
+// 	$('#btnCataSee').onclick=function(){
+// 		chrome.storage.sync.get(null,function(result){
+// 			debugger
+// 		})
+// 	}
+// }
 
 function initNav(){
 	var	$nav=document.querySelectorAll('.main .nav'),
@@ -107,15 +112,126 @@ function initData(){
 		for(i=0;i<cataArr.length;i++){
 			item=cataArr[i];
 			li=document.createElement('li');
+			li.id=item.id;
 			li.innerHTML=item.name;
 			li.onclick=function(){
-				var box=document.querySelector('.catabox');
-				box.addClass('active');
-				box.style.top=this.offsetTop+'px';
-				box.style.left=this.offsetLeft+'px';
+				showCatabox(this);
 			}
 			ulCatalog.appendChild(li);
 		}
 	})
+}
 
+/// 显示sitebox
+function showSitebox(ele){
+	
+}
+
+/// 显示catabox
+function showCatabox(ele){
+	var isCreate= ele.type==='button',
+	 	body=document.body,
+	    mask=document.createElement('div'),
+		bodyHeight=document.documentElement.scrollHeight,
+		bodyWidth=document.documentElement.scrollWidth,
+		tabCatalog=document.querySelector('#tabCatalog'),
+		catabox=document.createElement('div'),
+		btnCataSave,btnCataCancel,btnClose;
+
+	mask.id='mask';
+	body.appendChild(mask);
+	mask.style.height=bodyHeight+'px';
+	mask.style.width=bodyWidth+'px';
+
+	catabox.className='catabox active';
+	catabox.innerHTML=
+				'<h3 class="title">'+(isCreate?'新建分类':'编辑分类')+'</h3>'+
+				'<span class="close">x</span>'+
+				'<input type="text" class="name" placeholder="catalog name" '+(isCreate?'':'value="'+ele.innerHTML+'"')+'/>'+
+				'<div class="button-wrap">'+
+					'<button type="button" class="btn-ok" id="btnCataSave">确定</button>'+
+					'<button type="button" id="btnCataCancel">取消</button>'+
+					'<button type="button" style="display:none;" id="btnCataSee">看看</button>'+
+				'</div>';
+	body.appendChild(catabox);
+
+	catabox.style.top=getElementOffsetTop(ele)+'px';
+	catabox.style.left=getElementOffsetLeft(ele)+'px';
+	// catabox.addClass('show');
+
+	btnCataSave=document.querySelector('.catabox #btnCataSave');
+	btnCataCancel=document.querySelector('.catabox #btnCataCancel');
+	btnClose=document.querySelector('.catabox .close');
+
+	// 保存
+	btnCataSave.onclick=function(){
+		var name=document.querySelector('.catabox .name').value,
+			id,
+			obj={};
+
+		if(name===''){
+			alert('请输入catalog name');
+			return;
+		}	
+
+		// 新建保存
+		if(isCreate){
+			obj[name]={
+				id:name,
+				name:name,
+				type:'cata'
+			}
+			chrome.storage.sync.set(obj,function(){
+				// alert('set okay');
+				body.removeChild(mask);
+				body.removeChild(catabox);
+			})
+
+		}
+		// 编辑保存
+		else{
+			id=ele.id;
+			obj[id]={
+				id:id,
+				name:name,
+				type:'cata'
+			};
+
+			chrome.storage.sync.set(obj,function(){
+				// alert('set okay');
+				body.removeChild(mask);
+				body.removeChild(catabox);
+				ele.innerHTML=name;
+			})
+		}
+
+	}
+
+	// 关闭编辑框
+	btnCataCancel.onclick=btnClose.onclick=mask.onclick=function(){
+		body.removeChild(mask);
+		body.removeChild(catabox);
+	}
+}
+
+// 获取元素Offset Top
+function getElementOffsetTop(ele){
+	var actualTop=ele.offsetTop,
+		current=ele.offsetParent;
+	while(current!==null){
+		actualTop+=current.offsetTop;
+		current=current.offsetParent;
+	}
+	return actualTop;
+}
+
+// 获取元素Offset Left
+function getElementOffsetLeft(ele){
+	var actualLeft=ele.offsetLeft,
+		current=ele.offsetParent;
+	while(current!==null){
+		actualLeft+=current.offsetLeft;
+		current=current.offsetParent;
+	}
+	return actualLeft;
 }
